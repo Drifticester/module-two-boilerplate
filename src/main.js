7e169eaf1ef9e394e9741cdb79928936c7389232
Drifticester <drift-ice@tut.by>
2016-09-29 15:58:01 +0400

    Update main.js

--- src/main.js
+++ src/main.js
@@ -13,6 +13,27 @@ It will be passed automatically via proxy server
 function loadUsers(username) {
   const url = `${API_PROXY_URL}/${GAME}/account/list/?search=${username}`
   // create request to the url and return a promise
+  fetch(url)
+  .then(function(response) {
+    return response.json();
+   })
+  .then(function(data) {
+    renderSearchResult(data);
+  })
+  .catch( alert );
+}
+
+function loadUserInfo(userid) {
+  const url = `${API_PROXY_URL}/${GAME}/account/info/?account_id=${userid}`
+  // create request to the url and return a promise
+  fetch(url)
+  .then(function(response) {
+    return response.json();
+   })
+  .then(function(data) {
+    renderUserInfo(data.data[userid]);
+  })
+  .catch( alert );
 }
 
 function renderSpinner(domNode) {
@@ -24,8 +45,29 @@ function renderSearchResult(accounts) {
   // Note! it's already exist. See index.html for more info.
   // Each search result item should be rendered
   // inside node with `search-results_item` class name.
+  let users = document.getElementById('search-results');
+  let html = '';
+  accounts.data.forEach(function(item) {
+  	html += `<li data-user-id=${item.account_id}>${item.nickname}</li>`
+  })
+  users.innerHTML = html;
+  users.addEventListener('click', function(e) {
+  	var li = e.target;
+	loadUserInfo(e.target.getAttribute('data-user-id'));
+  })
+}
+
+function renderUserInfo(data) {
+	console.log(data);
+	let userInfo = document.getElementById('user-info');
+	userInfo.innerHTML = `Global Rating: ${data.global_rating}`;
 }
 
 document.addEventListener('DOMContentLoaded', () => {
   // add search button click handler here
+  let searchBtn = document.getElementById('search');
+  let username = document.getElementById('username');
+  searchBtn.addEventListener('click', function() {
+  	loadUsers(username.value);
+  });
 })
